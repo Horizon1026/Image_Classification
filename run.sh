979eb3e0f7ac4ba0b57b74e5d0d0668d0019848e
Horizon1026 <lijiapeng_atlantis@163.com>
2025-02-18 23:38:09 +0800

    update code

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@ if [ ! -d "./output" ]; then
     mkdir output
 fi
 
+python3 ./src/mnist_mlp/mnist_mlp.py
+python3 ./src/mnist_cnn/mnist_cnn.py
+
 cd ./build
 ./test_libtorch_mlp_mnist
 ./test_libtorch_cnn_mnist
