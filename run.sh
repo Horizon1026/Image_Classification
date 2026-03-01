b6fedb74040bf2862ec039746722fb4ac97f2340
Horizon1026 <lijiapeng_atlantis@163.com>
2026-03-01 16:04:35 +0800

    update code

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python3 ./src/train_model.py --model mlp --dataset_name mnist --max_epochs 10 --pretrained_model_path ./output/final_model.pth
-python3 ./src/train_model.py --model cnn --dataset_name mnist --max_epochs 10 --pretrained_model_path ./output/final_model.pth
-python3 ./src/train_model.py --model resnet --dataset_name mnist --max_epochs 10 --pretrained_model_path ./output/final_model.pth
-python3 ./src/train_model.py --model vit --dataset_name mnist --max_epochs 10 --pretrained_model_path ./output/final_model.pth
+python3 ./src/train_model.py --model mlp --dataset_dir /media/horizon/Database/robotic_datasets/visual_learning --dataset_name mnist --max_epochs 100 --pretrained_model_path ./output/final_model.pth
+python3 ./src/train_model.py --model cnn --dataset_dir /media/horizon/Database/robotic_datasets/visual_learning --dataset_name mnist --max_epochs 100 --pretrained_model_path ./output/final_model.pth
+python3 ./src/train_model.py --model resnet --dataset_dir /media/horizon/Database/robotic_datasets/visual_learning --dataset_name mnist --max_epochs 100 --pretrained_model_path ./output/final_model.pth
+python3 ./src/train_model.py --model vit --dataset_dir /media/horizon/Database/robotic_datasets/visual_learning --dataset_name mnist --max_epochs 100 --pretrained_model_path ./output/final_model.pth
